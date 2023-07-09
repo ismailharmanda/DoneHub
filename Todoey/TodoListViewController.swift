640e8824248b009a67ca23a609ac390525a9b203
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-09 17:57:34 +0300

    create a hardcoded tableview

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -2,17 +2,31 @@
 //  ViewController.swift
 //  Todoey
 //
-//  Created by Philipp Muellauer on 02/12/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
+//  Created by ismail harmanda on 09/07/2023.
+//  Copyright © 2023 ismail harmanda. All rights reserved.
 //
 
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //MARK:  TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 
 
